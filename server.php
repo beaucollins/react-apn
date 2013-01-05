65afebe56ac3ddb119678a32ba7fbf5cc2837c47
Beau Collins <beaucollins@gmail.com>
2013-01-04 17:30:24 -0800

    adding client cert

--- server.php
+++ server.php
@@ -2,13 +2,15 @@
 
 require 'vendor/autoload.php';
 
+$cert = realpath('client.cer');
+
 $loop = React\EventLoop\Factory::create();
 echo "Create connection" . PHP_EOL;
 $client = stream_socket_client('tcp://gateway.sandbox.push.apple.com:2195');
+stream_context_set_option( $client, 'ssl', 'local_cert', $cert );
 echo "Opened client" . PHP_EOL;
 $conn = new React\Socket\Connection($client, $loop);
 $conn->pipe(new React\Stream\Stream(STDOUT, $loop));
-$conn->write("Hello World!\n");
 
 echo "Running" . PHP_EOL;
 
